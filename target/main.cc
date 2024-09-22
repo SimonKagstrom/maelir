76713f856efbe555d55f4205d74d4feb3f163f5c
Simon Kagstrom <simon.kagstrom@gmail.com>
2024-09-22 19:54:32 +0200

    target: build: Comment with test for strangeness...

--- target/main.cc
+++ target/main.cc
@@ -15,10 +15,10 @@ extern "C" void
 app_main(void)
 {
     auto display = std::make_unique<DisplayTarget>();
-    //auto gps = std::make_unique<UartGps>(UART_NUM_1,
-    //                                     17,  // RX -> A0
-    //                                     16); // TX -> A1
-    auto gps = std::make_unique<GpsSimulator>();
+    auto gps = std::make_unique<UartGps>(UART_NUM_1,
+                                         17,  // RX -> A0
+                                         16); // TX -> A1
+    //auto gps = std::make_unique<GpsSimulator>();
 
     auto gps_reader = std::make_unique<GpsReader>(*gps);
     auto producer = std::make_unique<TileProducer>(gps_reader->AttachListener());
@@ -26,7 +26,7 @@ app_main(void)
     auto ui = std::make_unique<UserInterface>(
         *producer, *display, gps_reader->AttachListener(), route_service->AttachListener());
 
-    gps->Start(0);
+    //gps->Start(0);
     gps_reader->Start(0);
     producer->Start(0);
     route_service->Start(0);
