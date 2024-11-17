c56bc74a451d79e743fc10ab031a524a81e3300f
Simon Kagstrom <simon.kagstrom@gmail.com>
2024-11-17 09:40:02 +0100

    target: Remove old i2c/expander code

--- target/main/target_display/target_display.cc
+++ target/main/target_display/target_display.cc
@@ -1,12 +1,5 @@
 #include "target_display.hh"
 
-extern "C" {
-#include "MyWire.h"
-#include "PCA9554.h"
-}
-
-#include "init_operations.hpp"
-
 #include <cstring>
 #include <driver/i2c_master.h>
 #include <driver/spi_master.h>
@@ -59,12 +52,6 @@ constexpr auto kExpanderTftSck = IO_EXPANDER_PIN_NUM_0;
 constexpr auto kExpanderTftMosi = IO_EXPANDER_PIN_NUM_7;
 
 constexpr auto kTag = "target_display";
-
-constexpr auto init_operations =
-    hal::kDisplayWidth == 480 ? TL028WVC01_init_operations : hd40015c40_init_operations;
-constexpr auto init_operations_size = hal::kDisplayWidth == 480
-                                          ? sizeof(TL028WVC01_init_operations)
-                                          : sizeof(hd40015c40_init_operations);
 
 bool
 OnVsync(esp_lcd_panel_handle_t panel, const esp_lcd_rgb_panel_event_data_t* edata, void* user_ctx);
@@ -211,9 +198,6 @@ const st7701_lcd_init_cmd_t xTL028WVC01_init_operations[] = {
     // {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x12}, 5, 0}, /* This part of the parameters can be used for screen self-test */
     //{0xD1, (uint8_t []){0x81}, 1, 0},
     //{0xD2, (uint8_t []){0x08}, 1, 0},
-
-
-    //    {0x00, NULL, 0, 0} // End of commands
 };
 
 DisplayTarget::DisplayTarget()
@@ -362,75 +346,6 @@ DisplayTarget::DisplayTarget()
     };
     ESP_ERROR_CHECK(esp_lcd_new_panel_st7701(panel_io, &panel_config, &m_panel_handle));
 
-#if 0
-    my_wire_create();
-
-    MY_PCA9554_HANDLE handle;
-    my_PCA9554_create(&handle);
-
-    my_PCA9554_send_command(&handle, 0x01);
-    vTaskDelay(100);
-
-    my_PCA9554_batch(&handle, init_operations, init_operations_size);
-
-    esp_lcd_rgb_panel_config_t panel_config = {};
-
-    panel_config.data_width = 16; // RGB565 in parallel mode, thus 16bit in width
-    panel_config.psram_trans_align = 64;
-    panel_config.clk_src = LCD_CLK_SRC_DEFAULT;
-    panel_config.disp_gpio_num = GPIO_NUM_NC;
-    panel_config.pclk_gpio_num = TFT_PCLK;
-    panel_config.vsync_gpio_num = TFT_VSYNC;
-    panel_config.hsync_gpio_num = TFT_HSYNC;
-    panel_config.de_gpio_num = TFT_DE;
-    panel_config.data_gpio_nums[0] = TFT_B1;
-    panel_config.data_gpio_nums[1] = TFT_B2;
-    panel_config.data_gpio_nums[2] = TFT_B3;
-    panel_config.data_gpio_nums[3] = TFT_B4;
-    panel_config.data_gpio_nums[4] = TFT_B5;
-    panel_config.data_gpio_nums[5] = TFT_G0;
-    panel_config.data_gpio_nums[6] = TFT_G1;
-    panel_config.data_gpio_nums[7] = TFT_G2;
-    panel_config.data_gpio_nums[8] = TFT_G3;
-    panel_config.data_gpio_nums[9] = TFT_G4;
-    panel_config.data_gpio_nums[10] = TFT_G5;
-    panel_config.data_gpio_nums[11] = TFT_R1;
-    panel_config.data_gpio_nums[12] = TFT_R2;
-    panel_config.data_gpio_nums[13] = TFT_R3;
-    panel_config.data_gpio_nums[14] = TFT_R4;
-    panel_config.data_gpio_nums[15] = TFT_R5;
-
-    /*
-     * Ref https://github.com/espressif/esp-idf/[...]/i80_controller_example_main.c
-     *
-     *   "PCLK frequency can't go too high as the limitation of PSRAM bandwidth"
-     * 
-     * However, with the fill callback it can go up to high frequencies. For some
-     * reason, this also affect backlight flickering.
-     */
-    panel_config.timings.pclk_hz = 17 * 1000 * 1000;
-    panel_config.timings.h_res = hal::kDisplayWidth;
-    panel_config.timings.v_res = hal::kDisplayHeight;
-    panel_config.timings.hsync_back_porch = 44;
-    panel_config.timings.hsync_front_porch = 50;
-    panel_config.timings.hsync_pulse_width = 2;
-    panel_config.timings.vsync_back_porch = 18;
-    panel_config.timings.vsync_front_porch = 16;
-    panel_config.timings.vsync_pulse_width = 2;
-    panel_config.timings.flags.pclk_active_neg = 0;
-
-    panel_config.timings.flags.hsync_idle_low = 1;
-    panel_config.timings.flags.vsync_idle_low = 1;
-
-    // Bounce buffer
-    panel_config.bounce_buffer_size_px = hal::kDisplayWidth * 8;
-    panel_config.flags.no_fb = 1;
-    panel_config.flags.bb_invalidate_cache = 0;
-#endif
-
-
-    //    ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(&panel_config, &m_panel_handle));
-
     esp_lcd_rgb_panel_event_callbacks_t callbacks = {};
     callbacks.on_bounce_empty = DisplayTarget::OnBounceBufferFillStatic;
     callbacks.on_vsync = OnVsync;
@@ -444,10 +359,6 @@ DisplayTarget::DisplayTarget()
 
     // Turn on the backlight
     esp_io_expander_set_level(expander_handle, kExpanderTftBacklight, 1);
-
-
-    //    my_PCA9554_pin_mode(&handle, PCA_TFT_BACKLIGHT, OUTPUT);
-    //    my_PCA9554_digital_write(&handle, PCA_TFT_BACKLIGHT, HIGH);
 }
 
 uint16_t*
@@ -456,9 +367,6 @@ DisplayTarget::GetFrameBuffer()
     return m_frame_buffers[m_current_update_frame];
 }
 
-static std::atomic<int> fill_cnt = 0;
-static std::atomic<int> finish_cnt = 0;
-static std::atomic<int> vsync_cnt = 0;
 void IRAM_ATTR
 DisplayTarget::Flip()
 {
@@ -470,14 +378,12 @@ DisplayTarget::Flip()
 bool IRAM_ATTR
 OnVsync(esp_lcd_panel_handle_t panel, const esp_lcd_rgb_panel_event_data_t* edata, void* user_ctx)
 {
-    vsync_cnt++;
     return false;
 }
 
 void IRAM_ATTR
 DisplayTarget::OnBounceBufferFill(void* bounce_buf, int pos_px, int len_bytes)
 {
-    fill_cnt++;
     // Fill bounce buffer with the frame buffer data
     memcpy(bounce_buf,
            reinterpret_cast<const uint8_t*>(m_frame_buffers[!m_current_update_frame] + pos_px),
@@ -487,7 +393,6 @@ DisplayTarget::OnBounceBufferFill(void* bounce_buf, int pos_px, int len_bytes)
 void IRAM_ATTR
 DisplayTarget::OnBounceBufferFinish()
 {
-    finish_cnt++;
     if (m_flip_requested)
     {
         m_flip_requested = false;
