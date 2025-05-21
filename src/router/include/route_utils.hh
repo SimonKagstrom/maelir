079145e18a943d93ab3d386a1c8470108e69155c
Simon Kagstrom <simon.kagstrom@gmail.com>
2025-05-21 21:37:50 +0200

    route_utils: Don't printout with broken format string

--- src/router/include/route_utils.hh
+++ src/router/include/route_utils.hh
@@ -26,7 +26,6 @@ IsWater(std::span<const uint32_t> land_mask, IndexType index)
 {
     if (index / 32 >= land_mask.size())
     {
-        printf("Index out of bounds %d (%d) vs %zu\n", index, index / 32, land_mask.size());
         return false;
     }
 
