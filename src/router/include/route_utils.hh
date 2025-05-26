27833b36c71eb975dea16be591b7e3083df6c80c
Simon Kagstrom <simon.kagstrom@gmail.com>
2025-05-26 21:25:52 +0200

    route_utils: Inline a bit to avoid unused warnings

--- src/router/include/route_utils.hh
+++ src/router/include/route_utils.hh
@@ -8,20 +8,20 @@
 #include <cmath>
 #include <numbers>
 
-static IndexType
+static inline IndexType
 PointToLandIndex(Point point, unsigned row_size)
 {
     return (point.y / kPathFinderTileSize) * row_size + (point.x / kPathFinderTileSize);
 }
 
-static Point
+static inline Point
 LandIndexToPoint(IndexType index, unsigned row_size)
 {
     return {static_cast<int32_t>((index % row_size) * kPathFinderTileSize),
             static_cast<int32_t>((index / row_size) * kPathFinderTileSize)};
 }
 
-static bool
+static inline bool
 IsWater(std::span<const uint32_t> land_mask, IndexType index)
 {
     if (index / 32 >= land_mask.size())
@@ -32,7 +32,7 @@ IsWater(std::span<const uint32_t> land_mask, IndexType index)
     return (land_mask[index / 32] & (1 << (index % 32))) == 0;
 }
 
-static Vector
+static inline Vector
 IndexPairToDirection(IndexType from, IndexType to, unsigned row_size)
 {
     int from_x = from % row_size;
